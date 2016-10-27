fae3aa6a6954c48043cdadad5aed20dcc692a21b
Adam Hawyard <achayward1@gmail.com>
2016-10-27 12:20:23 -0400

    Saving for later. Added projects and bio info.

--- js/main.js
+++ js/main.js
@@ -50,3 +50,41 @@ var changeCurrentNav= function(el){
   $(".nav-link").removeClass("active");
   $(".nav-item .nav-link[href='"+$(el).attr("href")+"']").addClass("active");
 }
+
+var readMore= function(e){
+  $(e.target).siblings
+}
+//borrowed from codepen
+$(document).ready(function() {
+    // Configure/customize these variables.
+    var showChar = 100;  // How many characters are shown by default
+    var ellipsestext = "...&nbsp;";
+    var moretext = "See more >";
+    var lesstext = "< See less";
+
+
+    $('.more').each(function() {
+        var c = '<span class="moreellipses">' + ellipsestext+
+        '</span>&nbsp;<a class="morelink" href="#">'+
+        moretext + '</a>';
+        $(this).append(c);
+    });
+
+    $(".morelink").click(function(){
+        if($(this).hasClass("less")) {
+          $($(this).parent().siblings("p")[0]).append($('<span class="moreellipses"></span>').html(ellipsestext));
+          $($(this).parent().siblings("p")[0]).append(this);
+          $(this).removeClass("less");
+          $(this).html(moretext);
+          $(this).parent().siblings("p").css("display","none");
+        } else {
+          $($(this).siblings("span")[0]).remove();
+          $($(this).parent().siblings("p")[$(this).parent().siblings("p").length-1]).append(this);
+          $(this).addClass("less");
+          $(this).html(lesstext);
+          $(this).parent().css("display","block")
+          $(this).parent().siblings().css("display","block");
+        }
+        return false;
+    });
+});
