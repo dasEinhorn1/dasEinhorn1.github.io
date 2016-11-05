e40b6260dcf7f396931199e7a6a7a448b0fb8a95
Adam Hawyard <achayward1@gmail.com>
2016-11-05 16:45:19 -0400

    added more commands, styled, etc.

---
+++ theCometGame/js/jsDict.js
@@ -0,0 +1,110 @@
+/*
+  This is a dictionary class built by a fantastic user on stack overflow. I use
+  it to connect events to their elements on the page
+*/
+function JSdict() {
+    this.Keys = [];
+    this.Values = [];
+}
+
+// Check if dictionary extensions aren't implemented yet.
+// Returns value of a key
+if (!JSdict.prototype.getVal) {
+    JSdict.prototype.getVal = function (key) {
+      console.log(key);
+      console.log(this.Keys);
+        if (key == null) {
+            return false;
+        }
+        for (var i = 0; i < this.Keys.length; i++) {
+            if (this.Keys[i] == key) {
+                return this.Values[i];
+            }
+        }
+        return false;
+    }
+}
+
+
+// Check if dictionary extensions aren't implemented yet.
+// Updates value of a key
+if (!JSdict.prototype.update) {
+    JSdict.prototype.update = function (key, val) {
+        if (key == null || val == null) {
+            return "Key or Value cannot be null";
+        }
+        // Verify dict integrity before each operation
+        if (keysLength != valsLength) {
+            return "Dictionary inconsistent. Keys length don't match values!";
+        }
+        var keysLength = this.Keys.length;
+        var valsLength = this.Values.length;
+        var flag = false;
+        for (var i = 0; i < keysLength; i++) {
+            if (this.Keys[i] == key) {
+                this.Values[i] = val;
+                flag = true;
+                break;
+            }
+        }
+        if (!flag) {
+            return "Key does not exist";
+        }
+    }
+}
+
+// Check if dictionary extensions aren't implemented yet.
+// Adds a unique key value pair
+if (!JSdict.prototype.add) {
+    JSdict.prototype.add = function (key, val) {
+        // Allow only strings or numbers as keys
+        if (typeof (key) == "number" || typeof (key) == "string") {
+            if (key == null || val == null) {
+                return "Key or Value cannot be null";
+            }
+            if (keysLength != valsLength) {
+                return "Dictionary inconsistent. Keys length don't match values!";
+            }
+            var keysLength = this.Keys.length;
+            var valsLength = this.Values.length;
+            for (var i = 0; i < keysLength; i++) {
+                if (this.Keys[i] == key) {
+                    return "Duplicate keys not allowed!";
+                }
+            }
+            this.Keys.push(key);
+            this.Values.push(val);
+        }
+        else {
+            return "Only number or string can be key!";
+        }
+    }
+}
+
+// Check if dictionary extensions aren't implemented yet.
+// Removes a key value pair
+if (!JSdict.prototype.remove) {
+    JSdict.prototype.remove = function (key) {
+        if (key == null) {
+            return "Key cannot be null";
+        }
+        if (keysLength != valsLength) {
+            return "Dictionary inconsistent. Keys length don't match values!";
+        }
+        var keysLength = this.Keys.length;
+        var valsLength = this.Values.length;
+        var flag = false;
+        for (var i = 0; i < keysLength; i++) {
+            if (this.Keys[i] == key) {
+                this.Keys.shift(key);
+                this.Values.shift(this.Values[i]);
+                flag = true;
+                break;
+            }
+        }
+        if (!flag) {
+            return "Key does not exist";
+        }
+    }
+}
+// -----------------------------------------------------------------------------
--- theCometGame/js/world.js
+++ theCometGame/js/world.js
@@ -1,13 +1,92 @@
-var Room= function(nm, desc){
+var Room= function(nm, desc, silence="Nobody can hear you.", exits=[], npcs=[]){
   this.name=nm;
   this.description=desc;
+  this.silence=silence;
+  this.exits=exits;
+  this.npcs=npcs;
 }
 
 Room.prototype.enterText = function () {
-  return [["You have entered the "+this.name,this.description],"locator"]
+  return ["You have entered the "+this.name+"... "+this.description,"locator"]
 };
 Room.prototype.describe = function () {
-  return [["You are in the "+this.name,this.description],"locator"]
+  outputResult(["You are in the "+this.name+"... "+this.description,"locator"]);
+  outputResult(this.getExits());
+  return(this.getSayings());
 };
 
+Room.prototype.respond=function(said) {
+  if(this.npcs.length==0){
+    return [this.silence,"locator"]
+  }
+}
+Room.prototype.getSayings=function(said) {
+  if(this.npcs.length==0){
+    return ["Sayings: There's nobody around, say what you want",'locator'];
+  }
+}
+Room.prototype.addExit=function(name){
+  this.exits.push(name);
+}
+Room.prototype.getExits=function () {
+  if(this.exits.length==0){
+    return ["It seems you may be stuck here...","locator"];
+  }else{
+    var exts="";
+    for(var i=1;i<this.exits.length+1;i++){
+      exts+=" "+i+") "+this.exits[i-1]+" |"
+    }
+    return ["Destinations:"+exts.slice(0,exts.length-2),"locator"]
+  }
+}
+
+Room.prototype.checkExits=function(name){
+  if(this.exits.includes(name)){
+    return name;
+  }else if(!isNaN(parseInt(name))){
+    try{
+      return this.exits[parseInt(name)-1];
+    }catch(e){
+      return false
+    }
+  }else{
+    return false;
+  }
+}
+
+
+var World= function(){
+  this.rooms=new JSdict();
+  this.currentRoom=null;
+  this.lastRoom=null;
+}
+
+World.prototype.addRoom=function(r){
+  this.rooms.add(r.name,r);
+}
+World.prototype.changeRooms=function(name){
+  if(this.lastRoom!=null && name=="back"){
+    console.log("back");
+    name=this.lastRoom;
+  }
+  var nm=this.currentRoom.checkExits(name);
+  if(nm){
+    this.lastRoom=this.currentRoom.name;
+    this.currentRoom=this.rooms.getVal(nm);
+    return this.currentRoom.enterText();
+  }else{
+    return ["You can't go that way.","error"];
+  }
+}
+World.prototype.setCurrentRoom=function(name){
+  this.currentRoom=this.rooms.getVal(name);
+}
+var w=new World();
 var testRoom= new Room("testing room", "It fills you with a sense of despair.");
+testRoom.addExit("other testing room");
+var nextRoom= new Room("other testing room", "It smells like oranges in here!",
+"Your words echo off the orange scented walls. You are all alone");
+nextRoom.addExit("testing room");
+w.addRoom(testRoom);
+w.addRoom(nextRoom);
+w.setCurrentRoom("testing room");
